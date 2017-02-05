7c5f214feb90c1e5010314bd5707567a927afb72
mat1489 <mat1489@op.pl>
2017-02-05 13:19:45 +0100

    3-nodes

--- 3-nodes/js/script.js
+++ 3-nodes/js/script.js
@@ -1,21 +1,37 @@
 "use strict";
 
-var parFirst = document.getElementById('parFirst');
-console.log(parFirst);
+var pierwszyDiv= document.getElementById('parFirst');
+console.log(pierwszyDiv.parentNode);
+console.log(pierwszyDiv.parentElement);
+
+console.log(pierwszyDiv.childNodes);
+console.log(pierwszyDiv.children);          //tablica do konkretnego elementu children[0]
 
 
-var linki= document.getElementsByClassName('link');
-console.log(linki);
+console.log(pierwszyDiv.firstChild);
+console.log(pierwszyDiv.lastChild);
 
-var linkiPoTagu=document.getElementsByTagName('a');
-console.log(linkiPoTagu);
+var pierwszyChild=pierwszyDiv.childNodes[0];
+console.log(pierwszyChild);
+console.log(pierwszyChild.nextSibling.nextSibling.nextSibling);//wer1
+console.log(pierwszyDiv.childNodes[5]);                         //wer2
 
-var linkiPoSelektorze=document.querySelectorAll('.superlink');
-console.log(linkiPoSelektorze);
 
-var paragrafPoId=document.querySelector('#parSecond');
+var childNodesDiva = pierwszyDiv.childNodes;
 
-var pierwszyLinkPoSelektorze= document.querySelector('.superlink');
+childNodesDiva.forEach(function (element){
+    if(element.nodeType == 3 ){
+        console.log(element);
+    }
+});
 
+
+var childrenDiva =  pierwszyDiv.children;
+console.log(childrenDiva);
+console.log(childrenDiva.length);
+
+for (var i=0; i <childrenDiva.length; i++){
+    console.log(childrenDiva[i]);
+}
 
 
